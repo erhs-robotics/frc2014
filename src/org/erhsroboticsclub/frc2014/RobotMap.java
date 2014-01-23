3dfe06e9bb5287766004c197ebc703546551fa46
Nick Yaculak <nyaculak@gmail.com>
2014-01-22 21:03:38 -0500

    adding RobotMap class

---
+++ src/org/erhsroboticsclub/frc2014/RobotMap.java
@@ -0,0 +1,17 @@
+package org.erhsroboticsclub.frc2014;
+
+/**
+ * A class to contain static fields to easily reference input/output ports and
+ * universal robot constants.
+ */
+public class RobotMap {
+    
+    // PWM Output
+    
+    // Digital Input
+    
+    // Analog Input
+    
+    // USB Input (Joysticks)
+    
+}
