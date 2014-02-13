985c69bb317fd166462363155ae8b008ba646e9d
Nick Yaculak <nyaculak@gmail.com>
2014-02-13 18:23:27 -0500

    adding debug boilerplate

--- src/org/erhsroboticsclub/frc2014/RobotDebug.java
+++ src/org/erhsroboticsclub/frc2014/RobotDebug.java
@@ -6,11 +6,32 @@ package org.erhsroboticsclub.frc2014;
  */
 public class RobotDebug {
     
-    Robot robot;
+    private Robot robot;
     
     public RobotDebug(Robot robot) {
         this.robot = robot;
     }
     
+    public void drive() {
+        if (!robot.stick.getRawButton(1)) {
+            robot.drive.mecanumDrive_Cartesian(robot.stick.getX(), robot.stick.getY(), 
+                                               0, robot.gyro.getAngle());
+        } else {
+            robot.drive.mecanumDrive_Cartesian(robot.stick.getX(), robot.stick.getY(), 
+                                               robot.stick.getZ(), robot.gyro.getAngle());
+        }
+    }
+    
+    public void winch() {
         
+    }
+    
+    public void release() {
+        
+    }
+    
+    public void collector() {
+        
+    }
+    
 }
