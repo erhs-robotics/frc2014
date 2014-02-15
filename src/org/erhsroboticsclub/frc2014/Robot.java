3dcbf8818186dfce21125ea5441c212944207ada
Nick Yaculak <nyaculak@gmail.com>
2014-02-15 17:32:11 -0500

    declaring talons within the RobotDrive instantiation

--- src/org/erhsroboticsclub/frc2014/Robot.java
+++ src/org/erhsroboticsclub/frc2014/Robot.java
@@ -6,8 +6,9 @@ import org.erhsroboticsclub.frc2014.utilities.Messenger;
 public class Robot extends SimpleRobot {
 
     // Motors and motor control
-    Talon topLeftMotor, bottomLeftMotor, topRightMotor, bottomRightMotor;
     RobotDrive drive;
+    
+    // Joysticks
     Joystick stick;
     
     // Sensor inputs
@@ -18,12 +19,10 @@ public class Robot extends SimpleRobot {
 
     public void robotInit() {
         // Motors and motor control
-        topLeftMotor     = new Talon(RobotMap.TOP_LEFT_MOTOR);
-        bottomLeftMotor  = new Talon(RobotMap.BOTTOM_LEFT_MOTOR);
-        topRightMotor    = new Talon(RobotMap.TOP_RIGHT_MOTOR);
-        bottomRightMotor = new Talon(RobotMap.BOTTOM_RIGHT_MOTOR);
-        drive = new RobotDrive(topLeftMotor, bottomLeftMotor, 
-                               topRightMotor, bottomRightMotor);
+        drive = new RobotDrive(new Talon(RobotMap.TOP_LEFT_MOTOR),
+                               new Talon(RobotMap.BOTTOM_LEFT_MOTOR),
+                               new Talon(RobotMap.TOP_RIGHT_MOTOR),
+                               new Talon(RobotMap.BOTTOM_RIGHT_MOTOR));
         stick = new Joystick(RobotMap.DRIVE_JOYSTICK);
         
         // Sensor inputs
